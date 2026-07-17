b0239d12a0278d8e49ab78d64dacb459b2418464
AdzraNurulAditama <adzraaditama06@gmail.com>
2026-07-17 15:10:16 +0700

    Database: Mengisi seeder education

--- database/seeders/EducationSeeder.php
+++ database/seeders/EducationSeeder.php
@@ -2,16 +2,27 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Education;
 
 class EducationSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        // 1. Pendidikan Tinggi (Kuliah)
+        Education::create([
+            'institution' => 'Telkom University',
+            'degree' => 'D3 Sistem Informasi',
+            'period' => '2024 - Sekarang',
+            'description' => 'Mempelajari dasar-dasar pengembangan sistem informasi, UI/UX design, manajemen basis data (MySQL), pemrograman web (PHP/Laravel), serta pengembangan solusi digital tingkat dasar.'
+        ]);
+
+        // 2. Pendidikan Menengah Atas (SMA)
+        Education::create([
+            'institution' => 'SMAN 4 Karawang',
+            'degree' => 'Matematika dan Ilmu Pengetahuan Alam',
+            'period' => '2021 - 2024',
+            'description' => 'Fokus pada peminatan sains dan matematika. Aktif memimpin organisasi kesiswaan ekstrarkurikuler dan kepanitiaan seminar sekolah.'
+        ]);
     }
 }
